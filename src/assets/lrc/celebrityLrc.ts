7fb3b071a6aa362f16b2b2096608b728cba88ceb
MinChan Lee <shigatsu970704@gmail.com>
2021-03-25 16:55:03 +0900

    Add Celebrity LRC

---
+++ src/assets/lrc/celebrityLrc.ts
@@ -0,0 +1,57 @@
+﻿export default `
+[00:00.00]Artist - IU & Song - Celebrity
+[00:01.12]세상의 모서리
+[00:03.60]구부정하게 커버린 골칫거리 outsider
+[00:10.72]걸음걸이‚ 옷차림
+[00:13.12]이어폰 너머 playlist 음악까지 다 minor
+[00:20.41]넌 모르지 떨군 고개 위
+[00:24.81]환한 빛 조명이 (어딜 비추는지)
+[00:29.31]느려도 좋으니 결국 알게 되길
+[00:34.51]The one and only
+[00:37.31]You are my celebrity
+[00:39.81]잊지마 넌 흐린 어둠 사이
+[00:45.01]왼손으로 그린 별 하나
+[00:49.51]보이니 그 유일함이 얼마나
+[00:54.61]아름다운지 (말야)
+[00:57.00]You are my celebrity
+[00:59.31](I know you are‚ are)
+[01:01.42](I know you are‚ are)
+[01:03.81](I know you are‚ you are‚ are)
+[01:07.00]Celebrity
+[01:08.41](I know you are‚ are)
+[01:11.00](I know you are‚ are)
+[01:13.41](I know you are‚ are)
+[01:15.91]You are my celebrity
+[01:18.01]지쳐버린 표정 마치
+[01:20.21]전원을 꺼놓은 듯이 심장 소린 too quiet
+[01:27.22]네가 가진 반짝거림
+[01:29.71]상상력‚ identity까지 모조리 diet‚ huh
+[01:37.11]넌 모르지 아직 못다 핀
+[01:41.70]널 위해 쓰여진 (오래된 사랑시)
+[01:46.10]헤매도 좋으니 웃음 짓게 되길
+[01:51.00]The one and only
+[01:53.91]You are my celebrity
+[01:56.50]잊지마 넌 흐린 어둠 사이
+[02:01.70]왼손으로 그린 별 하나
+[02:06.21]보이니 그 유일함이 얼마나
+[02:11.32]아름다운지 (말야)
+[02:13.82]You are my celebrity
+[02:16.01]발자국마다 이어진 별자리
+[02:20.20]그 서투른 걸음이 새겨놓은 밑그림
+[02:25.82]오롯이 너를 만나러 가는 길
+[02:30.00]그리로 가면 돼 점선을 따라
+[02:35.21]잊지마 이 오랜 겨울 사이
+[02:40.01]언 틈으로 피울 꽃 하나
+[02:44.71]보이니 하루 뒤 봄이 얼마나
+[02:49.72]아름다울지 (말야)
+[02:52.10]You are my celebrity
+[02:54.20](I know you are‚ arе)
+[02:56.61](I know you are‚ are)
+[02:58.90](I know you are‚ you arе‚ are)
+[03:01.62]Celebrity
+[03:03.70](I know you are‚ are)
+[03:06.12](I know you are‚ are)
+[03:08.41](I know you are‚ are)
+[03:10.91]You are my celebrity
+[03:14.50]by RentAnAdviser.com
+`;
